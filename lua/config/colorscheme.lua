032395048c745cff9e5ebe0bbe0602097990d3ff
gschri <gabrielschri@gmail.com>
2022-05-19 11:17:34 -0300

    mend

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,3 +1,9 @@
 -- colorscheme.lua
 -- set colorscheme
+vim.g.tokyonight_style = 'storm'
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_italic_keywords = true
+vim.g.tokyonight_italic_comments = true
+vim.g.tokyonight_sidebars = {"qf","vista_kind","terminal","packer"}
+
 vim.cmd[[colorscheme tokyonight]]
