a4a9aa07f1fbc0c5c8cf4e0f5a957dc72afdc2ac
gschri <gabrielschri@gmail.com>
2022-05-19 13:22:56 -0300

    Add nvim-ts-autotag for closing and opening automatically html tags and react components among other things

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -37,6 +37,8 @@ return require('packer').startup(function()
   use 'L3MON4D3/LuaSnip'
   -- Syntax highlighting
   use 'nvim-treesitter/nvim-treesitter'
+  -- autoclose and autorename html tag (requires treesitter)
+  use 'windwp/nvim-ts-autotag' 
   -- Autopairs for nvim
   use 'windwp/nvim-autopairs'
 	-- Fuzzy finder
